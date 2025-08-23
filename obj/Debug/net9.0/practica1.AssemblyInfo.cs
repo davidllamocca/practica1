d9a114f85308e8a2b58267c7c4fcad54292a9073
davidllamocca <carlos_llamocca@usmp.pe>
2025-08-23 16:03:47 -0500

    estructura

--- obj/Debug/net9.0/practica1.AssemblyInfo.cs
+++ obj/Debug/net9.0/practica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd6abbcc69131273fec128eb3b61a4ca7fb998d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29f86da1bcf4277477a1d451eb56c2578902392")]
 [assembly: System.Reflection.AssemblyProductAttribute("practica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
